d8ad3e498a09cc88a69f31eec387556a60ad588d
OrneryCash <60810240+OrneryCash@users.noreply.github.com>
2024-02-23 14:47:57 +0700

    average.cpp completed

--- average.cpp
+++ average.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
 
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    cout << "Please input numbers to find average.\n";
+    return 1;
+  }
+  double sum = 0;
+  int n = argc - 1;
+  for (int i = 1; i <= n; i++) {
+    sum += atof(argv[i]);
+  }
+  cout << "---------------------------------\n";
+  cout << "Average of " << n << " numbers = " << sum / n << '\n';
+  cout << "---------------------------------\n";
+  return 0;
+}
